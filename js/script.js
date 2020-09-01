6b7ddb5a9af958d962e96a98f5757e530b451c38
Lau <arinel88@gmail.com>
2020-09-01 09:48:13 +0200

    Inserito switch

--- js/script.js
+++ js/script.js
@@ -6,8 +6,29 @@
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-var numBombeArray = 16;
-var tentativi = 100;
+var numBombeArray = 3;
+var tentativi=6;
+
+// Livelli di difficoltà
+
+var difficoltà = parseInt(prompt("Inserisci il livello di difficoltà: 0, 1, 2"));
+
+switch (difficoltà) {
+  case 0:
+  tentativi = 6;
+    break;
+  case 1:
+  tentativi = 5;
+    break;
+  case 2:
+  tentativi = 4;
+    break;
+  default:
+  tentativi = 6;
+
+}
+
+var livello = tentativi - numBombeArray;
 
 // Funzione che genera un numero casuale da 1 a 100
 
@@ -50,33 +71,33 @@ var numUtenteArray = [];
 // Chiedo all'utente d'inserire un numero da 1 a 100 e faccio il check e lo pusho
 var bombaPresa = false;
 
-for (var i=0; i < (tentativi-numBombeArray) && bombaPresa == false; i++) {
-  console.log(tentativi-numBombeArray);
+for (var i=0; i < (livello) && bombaPresa == false; i++) {
+  console.log(livello);
   console.log("ciclo: " + i);
   do{
     var numUtente = parseInt(prompt("Inserisci un numero da 1 a 100. Good Luck ;)"));
 
-    if (checkArray(numUtenteArray, numUtente) == true) {
+    if (isNaN(numUtente) || numUtente > 100 || numUtente <1) {
+      alert("Attenzione, il valore che hai inserito non è un numero compreso tra 1 e 100!");
+    } else if (checkArray(numUtenteArray, numUtente) == true) {
       alert("Attenzione, hai già inserito questo numero!");
     }
-    // else (!isNaN(numUtente)) {
-    //   alert("Attenzione, il valore che hai inserito non è un numero!");
-    // }
+
   } while (checkArray(numUtenteArray, numUtente) == true);
   numUtenteArray.push(numUtente);
   console.log(numUtenteArray);
 
   // Controllo se la bomba è stata presa oppure no e vado avanti finché non vinco (forse)
 
-  if (checkArray(bombe, numUtenteArray[numUtenteArray.length - 1])){
-    alert("Hai preso una bomba!\n Hai perso :( \n Sei sfuggito alle bombe " + [numUtenteArray.length]);
+  if (checkArray(bombe, numUtenteArray[numUtenteArray.length - 1])) {
+    alert("Hai preso una bomba!\n Hai perso :( \n Sei sfuggito alle bombe " + [numUtenteArray.length] + " volte");
     bombaPresa = true;
   } else {
     alert("Bravo! Sei stato fortunato!");
   }
 }
 
-if (numUtenteArray.length == tentativi-numBombeArray){
-  alert("Complimenti! Hai vinto!\n Sei sfuggito alle bombe " + [numUtenteArray.length]);
+if (numUtenteArray.length == livello){
+  alert("Complimenti! Hai vinto!\n Sei sfuggito alle bombe " + [numUtenteArray.length] + " volte");
 }
 console.log("gioco finito");
